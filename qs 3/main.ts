9601114859d9e527b346fd5995c7c40d9b32d009
Burhanuddin <114940076+unkown60rola@users.noreply.github.com>
2024-03-10 02:44:39 +0500

    Add files via upload

---
+++ qs 3/main.ts
@@ -0,0 +1,4 @@
+let personName: string = "Burhanuddin";
+console.log(`lowercase : ${personName.tolowercase()}`);
+console.log(`uppercase : ${personName.touppercase()}`);
+console.log(`lowercase : ${personName.tolowercase()}`);
