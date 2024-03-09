61e64907386c2773a4d8fc398c674ea444ebd5e4
Burhanuddin <114940076+unkown60rola@users.noreply.github.com>
2024-03-10 02:41:55 +0500

    Add files via upload

---
+++ qs 2/Main.js
@@ -0,0 +1,4 @@
+var person = "Eric";
+console.log("Hello ".concat(person, ",would you like to learn some python today?"));
+var son = "Yousuf";
+console.log(son);
---
+++ qs 2/Main.ts
@@ -0,0 +1,2 @@
+let person: string= "Eric";
+console.log(`Hello ${person},would you like to learn some python today?`);
